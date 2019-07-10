be961362f025db747541192df8f33a1bbcd54c01
SergeyK-WebDeveloper <Sergey.Kist@gmail.com>
2019-07-10 08:39:40 +0300

    11_ написан базовый Класс для кэширования данных

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\Main;
+use vendor\core\App;
 
 /**
  * Description of Main
@@ -13,8 +14,19 @@ class MainController extends AppController{
 //    public $layout = 'main';
     
     public function indexAction(){
+//        App::$app->getList();
+        \R::fancyDebug(true);
         $model = new Main;
-        $posts = \R::findAll('posts');
+        
+        $posts = App::$app->cache->get('posts');
+        if(!$posts){
+            $posts = \R::findAll('posts');
+            App::$app->cache->set('posts', $posts, 3600*24);
+        }
+        
+//        echo date('Y-m-d H:i', time());
+//        echo '<br>';
+//        echo date('Y-m-d H:i', 1488105457);
         $post = \R::findOne('posts', 'id = 1');
         $menu = $this->menu;
         $title = 'PAGE TITLE';
---
+++ vendor/core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace vendor\core;
+
+use vendor\core\Registry;
+
+/**
+ * Description of App
+ *
+ */
+class App {
+    
+    public static $app;
+    
+    public function __construct() {
+        self::$app = Registry::instance();
+    }
+    
+}
